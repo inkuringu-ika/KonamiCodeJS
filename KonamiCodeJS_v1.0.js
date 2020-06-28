14c6e3945a83d697e24ab05ef8088e86f8dae258
inkuringu-ika <59644555+inkuringu-ika@users.noreply.github.com>
2020-06-29 01:31:50 +0900

    Add files

---
+++ KonamiCodeJS_v1.0.js
@@ -0,0 +1,34 @@
+/*
+Copyright (c) 2020 inkuringu-ika
+
+MIT License
+
+function konami_code_action(){
+//script
+};
+*/
+
+console.log("KonamiCodeJS v1.0 by inkuringu-ika");
+
+if(typeof konami_code_action == "function"){
+
+
+(function(){
+var input_code = [];
+var konami_code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+document.addEventListener("keyup",function(event){
+input_cmd.push(event.keyCode);
+if(input_code[input_code.length - 1] != konami_code[input_code.length - 1]){
+input_code = [];
+}else if(input_code.length == konami_code.length){
+konami_code_action();
+input_code = [];
+};
+});
+}());
+
+
+}else{
+//konami_code_action function is not defined
+throw "konami_code_action function is not defined.";
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# KonamiCode JavaScript
+You can use KonamiCode on your website!
+
+# How to use
+- Need to define a function named konami_code_action. This function will be called when the KonamiCode is entered.
+- Load this script into your website.
+- enjoy!
