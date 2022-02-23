dfcbbe4b49d9a9a78f0ea122a1af656e67c2faba
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-02-24 00:30:04 +0900

    It is now easier to handle, and the code detection mechanism has been improved.

--- KonamiCodeJS.js
+++ KonamiCodeJS.js
@@ -3,32 +3,25 @@ Copyright (c) 2020 typeling1578
 
 MIT License
 
-function konami_code_action(){
-//script
-};
+konamiCodeJS(function () {
+  console.log("KonamiCodeJS");
+})
 */
 
-console.log("KonamiCodeJS v1.0 by typeling1578");
+function konamiCodeJS(callback){
+    var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    var inputCode = [];
+    
+    window.addEventListener('keydown', function(event){
+        inputCode.push(event.keyCode);
 
-if(typeof konami_code_action == "function"){
+        if(inputCode.length > konamiCode.length){
+            inputCode.shift();
+        }
 
-
-(function(){
-var input_code = [];
-var konami_code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-document.addEventListener("keyup",function(event){
-input_cmd.push(event.keyCode);
-if(input_code[input_code.length - 1] != konami_code[input_code.length - 1]){
-input_code = [];
-}else if(input_code.length == konami_code.length){
-konami_code_action();
-input_code = [];
-};
-});
-}());
-
-
-}else{
-//konami_code_action function is not defined
-throw "konami_code_action function is not defined.";
-};
+        if(inputCode.toString() === konamiCode.toString()){
+            callback();
+            inputCode = [];
+        }
+    });
+}
